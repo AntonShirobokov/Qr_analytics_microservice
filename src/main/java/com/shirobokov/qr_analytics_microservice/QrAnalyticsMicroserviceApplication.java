934aaac173097bcd09da71f5e66de4ec0222d1b7
AntonShirobokov <shirobokov.anton2016@gmail.com>
2025-10-22 16:11:38 +0400

    Initial commit

---
+++ src/main/java/com/shirobokov/qr_analytics_microservice/QrAnalyticsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.shirobokov.qr_analytics_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrAnalyticsMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrAnalyticsMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shirobokov/qr_analytics_microservice/QrAnalyticsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shirobokov.qr_analytics_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrAnalyticsMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
